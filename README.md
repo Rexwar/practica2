8f2d769e61e00e9de8f0e04d9bc712d6b93ac342
Rexwar <reyvaldesm@gmail.com>
2023-11-15 15:42:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# practica-2
+ practica 2 con perfil react que consulta a api en laravel
---
+++ apiPerfil/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        $data = [
+            'Name' => 'Rey',
+            'Lastname' => 'Valdes Marangunic',
+            'Email' => 'reyvaldesm@gmail.com',
+            'City' => 'Calama',
+            'Country' => 'Chile',
+            'Summary' => 'Soy una persona resiliente, proactivo, atento, con ganas de mejorar dia a dia',
+            'Frameworks' => [
+                [
+                    'Name' => 'Laravel',
+                    'Level' => 'Principiante',
+                    'Year' => 2021
+                ],
+                // otros frameworks...
+            ],
+            'Hobbies' => [
+                [
+                    'Name' => 'Bicicleta',
+                    'Description' => 'ir a varios destinos en bicicleta, practicar trucos basicos como andar sin manos, cargado en un solo pedal, ejercitando equilibro'
+                ],
+                [
+                    'Name' => 'tech rabbit hole',
+                    'Description' => 'ver videos de divulgadores cientificos en youtube mayormente para enterarme del estado del arte de diversas areas de la tecnologia'
+                ],
+                [
+                    'Name' => 'rocket League',
+                    'Description' => 'practicar la ganancia de control en el juego, practicar trucos'
+                ],
+                // otros hobbies...
+            ]
+        ];
+
+        return response()->json($data);
+    }
+}
